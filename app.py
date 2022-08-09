a1373db990765bd8d9bf15bfc5258c54d7a48826
AryaBipin <110154938+AryaBipin@users.noreply.github.com>
2022-08-09 07:49:44 +0400

    g

--- app.py
+++ app.py
@@ -36,5 +36,4 @@ def upload_image():
     background.save(os.path.join(app.config['UPLOAD_FOLDER'], "profile.png"))
     return render_template('index.html')
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run()
